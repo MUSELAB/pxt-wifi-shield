aa6dd3718d2837c3c2aae0ac0a55f9aaf37c7f3b
Ken Lai <ken.lai@muselab.cc>
2018-03-28 11:30:25 +0800

    Update the at command

--- wifi.ts
+++ wifi.ts
@@ -44,15 +44,15 @@ namespace MuseIoT {
     }
 
     //% blockId=muselab_set_arcgis
-	//% block="Send Arcgis feature function %arcgisfunction|serviceid %featureserviceid|sensorid %sensorid|reading1 %reading1|reading2 %reading2|x %x|y %y|objectid(For update only) %objectid"
+	//% block="Send Arcgis feature function %arcgisfunction|serviceid %featureserviceid|sensorid %sensorid|x %x|y %y|reading1 %reading1|reading2 %reading2|objectid(For update only) %objectid"
 	//% weight=59	
-    export function sendArcgis(arcgisfunction: arcgisFunction, featureserviceid: string, sensorid: string, reading1: number, reading2: number, x: string, y: string, objectid: number): void {
+    export function sendArcgis(arcgisfunction: arcgisFunction, featureserviceid: string, sensorid: string,  x: string, y: string, reading1: number, reading2: number, objectid: number): void {
 		switch(arcgisfunction){
 			case arcgisFunction.add:
-                serial.writeLine("(AT+arcgis?featureserviceid="+featureserviceid+"&objectid=" + objectid +"&reading1="+reading1+"&reading2="+reading2+"&sensorid="+sensorid+"&x="+x+"&y="+y+")"); 
+                serial.writeLine("(AT+arcgis?arcgisfunction=add&featureserviceid="+featureserviceid+"&reading1="+reading1+"&reading2="+reading2+"&sensorid="+sensorid+"&x="+x+"&y="+y+")"); 
                 break
             case arcgisFunction.update:
-                serial.writeLine("(AT+arcgis?featureserviceid="+featureserviceid+"&objectid=" + objectid +"&reading1="+reading1+"&reading2="+reading2+"&sensorid="+sensorid+"&x="+x+"&y="+y+")"); 
+                serial.writeLine("(AT+arcgis?arcgisfunction=update&featureserviceid="+featureserviceid+"&objectid=" + objectid +"&reading1="+reading1+"&reading2="+reading2+"&sensorid="+sensorid+"&x="+x+"&y="+y+")"); 
                 break
 		}
     }
