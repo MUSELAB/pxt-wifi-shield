2d24f450ebda8fb3d94fae8b6e968d4ec47a1169
Muselab-official <daniel.bluhm@muselab.cc>
2018-09-08 14:01:46 +0800

    Added MuseWeather Temperature Block for HK and SG

--- wifi.ts
+++ wifi.ts
@@ -5,6 +5,8 @@ namespace MuseIoT {
 	let inbound2 = ""
 	let outbound1 = ""
 	let outbound2 = ""
+    let apiurlhk = "api.muselab.hk/hk/?id="
+    let apiurlsg = "api.muselab.hk/sg/?id="
 	
 	export enum arcgisFunction {
         //% block="Add"
@@ -46,6 +48,135 @@ namespace MuseIoT {
         bound1,
         //% block="2"
         bound2
+    }
+
+    export enum hkStationSelect {
+        //% blockId="HK1"
+        //% block="Hong Kong Observatory"
+        HK1 = 1,
+        //% blockId="HK2"
+        //% block="Kings Park"
+        HK2 = 2,
+		//% blockId="HK3"
+        //% block="Wong Chuk Hang"
+        HK3 = 3,
+        //% blockId="HK4"
+        //% block="Ta Kwu Ling"
+        HK4 = 4,
+		//% blockId="HK5"
+        //% block="Lau Fau Shan"
+        HK5 = 5,
+        //% blockId="HK6"
+        //% block="Sha Tin"
+        HK6 = 6,
+		//% blockId="HK7"
+        //% block="Tuen Mun"
+        HK7 = 7,
+        //% blockId="HK8"
+        //% block="Tseung Kwan O"
+        HK8 = 8,
+        //% blockId="HK9"
+        //% block="Sai Kung"
+        HK9 = 9,
+		//% blockId="HK10"
+        //% block="Cheung Chau"
+        HK10 = 10,
+        //% blockId="HK11"
+        //% block="Chek Lap Kok"
+        HK11 = 11,
+		//% blockId="HK12"
+        //% block="Tsing Yi"
+        HK12 = 12,
+        //% blockId="HK13"
+        //% block="Shek Kong"
+        HK13 = 13,
+		//% blockId="HK14"
+        //% block="Tsuen Wan Ho Koon"
+        HK14 = 14,
+        //% blockId="HK15"
+        //% block="Tsuen Wan Shing Mun Valley"
+        HK15 = 15,
+        //% blockId="HK16"
+        //% block="Hong Kong Park"
+        HK16 = 16,
+		//% blockId="HK17"
+        //% block="Shau Kei Wan"
+        HK17 = 17,
+        //% blockId="HK18"
+        //% block="Kowloon City"
+        HK18 = 18,
+		//% blockId="HK19"
+        //% block="Happy Valley"
+        HK19 = 19,
+        //% blockId="HK20"
+        //% block="Wong Tai Sin"
+        HK20 = 20,
+		//% blockId="HK21"
+        //% block="Stanley"
+        HK21 = 21,
+        //% blockId="HK22"
+        //% block="Kwun Tong"
+        HK22 = 22,
+        //% blockId="HK23"
+        //% block="Sham_Shui_Po"
+        HK23 = 23,
+		//% blockId="HK24"
+        //% block="Kai Tak Runway Park"
+        HK24 = 24,
+        //% blockId="HK25"
+        //% block="Yuen Long Park"
+        HK25 = 25,
+		//% blockId="HK26"
+        //% block="Tai Mei Tuk"
+        HK26 = 26
+    }
+
+    export enum sgStationSelect {
+        //% blockId="SG1"
+        //% block="Banyan"
+        SG1 = 1,
+        //% blockId="SG2"
+        //% block="Clementi"
+        SG2 = 2,
+		//% blockId="SG3"
+        //% block="EastCost Parkway"
+        SG3 = 3,
+        //% blockId="SG4"
+        //% block="Kim Chuan Road"
+        SG4 = 4,
+		//% blockId="SG5"
+        //% block="Marina Gardens Drive"
+        SG5 = 5,
+        //% blockId="SG6"
+        //% block="Nanyang Avenue"
+        SG6 = 6,
+		//% blockId="SG7"
+        //% block="Old Choa Chu Kang Road"
+        SG7 = 7,
+        //% blockId="SG8"
+        //% block="Pulau Ubin"
+        SG8 = 8,
+        //% blockId="SG9"
+        //% block="Sembawang Road"
+        SG9 = 9,
+		//% blockId="SG10"
+        //% block="Sentosa"
+        SG10 = 10,
+        //% blockId="SG11"
+        //% block="West Coast Highway"
+        SG11 = 11,
+		//% blockId="SG12"
+        //% block="Woodlands Avenue 9"
+        SG12 = 12,
+        //% blockId="SG13"
+        //% block="Woodlands Road"
+        SG13 = 13,
+		//% blockId="SG14"
+        //% block="Ang Mo Kio Avenue 5"
+        SG14 = 14,
+        //% blockId="SG15"
+        //% block="Scotts Road"
+        SG15 = 15
     }
 	
 	// -------------- 1. Initialization ----------------
@@ -411,6 +542,26 @@ namespace MuseIoT {
 	//% weight=14	
     export function setTurnOff(): void {
         serial.writeLine("(AT+deepsleep?time=0)");
+    }
+
+    //%subcategory=More
+    //%blockId=select_hkstation
+    //%block="Select Hong Kong Station %hkstation"
+	//% weight=13
+    export function getHkWeather(hkstation: hkStationSelect): string {
+        let hklink = "";
+        hklink = apiurlhk + hkstation;
+        return hklink;
+    }
+     
+    //%subcategory=More
+    //%blockId=select_sgstation
+    //%block="Select Singapore Station %sgstation"
+	//% weight=12
+    export function getSgWeather(sgstation: sgStationSelect): string {
+        let sglink = "";
+        sglink = apiurlsg + sgstation;
+        return sglink;
     }	
 
 }
